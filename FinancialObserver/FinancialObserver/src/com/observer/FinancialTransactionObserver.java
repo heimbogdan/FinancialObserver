3fdf694df83112668822790ee2f58cc3599ba282
heim.bogdan <heim.bogdan@hotmail.com>
2015-05-15 09:08:58 +0300

    financial obs

---
+++ FinancialObserver/FinancialObserver/src/com/observer/FinancialTransactionObserver.java
@@ -0,0 +1,22 @@
+package com.observer;
+
+import com.observer.interfaces.GeneralObserver;
+
+public class FinancialTransactionObserver implements GeneralObserver{
+
+	@Override
+	public void register() {
+		
+	}
+
+	@Override
+	public void unregister() {
+		
+	}
+
+	@Override
+	public void update() {
+		
+	}
+
+}
--- FinancialObserver/FinancialObserver/src/com/observer/interfaces/GeneralObserver.java
+++ FinancialObserver/FinancialObserver/src/com/observer/interfaces/GeneralObserver.java
@@ -1,5 +1,10 @@
 package com.observer.interfaces;
 
 public interface GeneralObserver {
-
+	
+	public void register();
+	
+	public void unregister();
+	
+	public void update();
 }
