243498efef30c18561ebca25fff40d538022f2d8
marius.ghimis <marius.ghimis@DMGHIMIS.hq.edata.ro>
2015-05-15 09:57:43 +0300

    ';l

---
+++ FinancialObserver/FinancialObserver/src/com/account/Account.java
@@ -0,0 +1,15 @@
+package com.account;
+
+public class Account {
+	public String accNo;
+
+	public String getAccNo() {
+		return accNo;
+	}
+
+	public void setAccNo(String accNo) {
+		this.accNo = accNo;
+	}
+	
+	
+}
